39584ad94b4e512c05a90bd20e56743f73064043
teleskop150750 <teleskop150750@gmail.com>
2020-12-12 19:53:00 +0300

    8

--- vendor/shop/core/base/View.php
+++ vendor/shop/core/base/View.php
@@ -11,15 +11,20 @@ abstract class View
     protected string $model;
     protected string $view;
     protected string $prefix;
+    protected ?string $layout;
     protected array $data = [];
     protected array $meta = [];
 
-    public function __construct(array $route, array $meta, string $layout = '', string $view = '')
+    public function __construct(array $route, array $meta, ?string $layout = '', string $view = '')
     {
         $this->route = $route;
         $this->controller = $route['controller'];
         $this->model = $route['controller'];
-        $this->view = $route['action'];
+        $this->view = $view;
         $this->prefix = $route['prefix'];
+        $this->meta = $meta;
+        if (is_null($layout)) {
+            $this->layout = $layout ?: LAYOUT;
+        }
     }
 }
