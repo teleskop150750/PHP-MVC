191a59bc017d954341d3f24cc6315de2dbc6c275
teleskop150750 <teleskop150750@gmail.com>
2020-12-13 00:54:28 +0300

    13-4

--- config/init.php
+++ config/init.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DEBUG", 1);
+define("DEBUG", 0);
 define("ROOT", dirname(__DIR__));
 define("WWW", ROOT . '/public');
 define("APP", ROOT . '/app');
@@ -10,11 +10,11 @@ define("CACHE", ROOT . '/tmp/cache');
 define("CONF", ROOT . '/config');
 define("LAYOUT", 'default');
 
-// http://teeskopShop/public/index.php
+// http://teleskop/public/index.php
 $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
-// http://teeskopShop/public/
+// http://teleskop/public/
 $app_path = preg_replace("#[^/]+$#", '', $app_path);
-// http://teeskopShop
+// http://teleskop
 $app_path = str_replace('/public/', '', $app_path);
 define("PATH", $app_path);
 define("ADMIN", PATH . '/admin');
--- public/errors/404.php
+++ public/errors/404.php
@@ -19,7 +19,7 @@
             <h1>404</h1>
             <h2>Page not found</h2>
         </div>
-        <a href="<?= WWW ?>">Homepage</a>
+        <a href="<?= PATH; ?>">Homepage</a>
     </div>
 </div>
 
