a06fd534ec903221b80e6a49192a33a72cdc921f
teleskop150750 <teleskop150750@gmail.com>
2020-12-19 16:59:06 +0300

    13-3

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,22 +1,26 @@
 <!doctype html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?= $this->getMeta(); ?>
 </head>
+
 <body>
-<h1>Шаблон По умолчанию</h1>
-<?= $content; ?>
+    <h1>Шаблон По умолчанию</h1>
+    <?= $content; ?>
 
-<?php
-$logs = \RedBeanPHP\R::getDatabaseAdapter()
-    ->getDatabase()
-    ->getLogger();
+    <?php
+    if (DEBUG) {
+        $logs = \RedBeanPHP\R::getDatabaseAdapter()
+            ->getDatabase()
+            ->getLogger();
 
-debug($logs->grep('SELECT'));
-?>
+        debug($logs->grep('SELECT'));
+    }
+    ?>
 </body>
+
 </html>
